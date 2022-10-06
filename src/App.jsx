39e93eedd9ec6d67bfd926d967389edd4006e90d
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-10-06 14:56:27 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <main className='grid min-h-screen place-content-center'>
+      <h1 className='text-3xl font-bold'>
+        React + Tailwind + Standard
+      </h1>
+    </main>
+  )
+}
+
+export default App
