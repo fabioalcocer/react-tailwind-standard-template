6ce0838db54c0c0dfea196b552da3d9893066617
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-10-06 15:59:47 -0400

    fix content to tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx'
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
-  },
-  plugins: [require('prettier-plugin-tailwindcss')]
+    extend: {},
+    plugins: [[require('prettier-plugin-tailwindcss')]]
+  }
 }
